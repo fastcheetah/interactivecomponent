5643747f6a9e08afbaf190cee2e3bf992f22afaf
Kelvin <edeh47245@gmail.com>
2025-02-12 23:44:46 +0100

    fix: Fixing bug where span elements dont return to their original color after being clicked

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.querySelectorAll('.number').forEach(span => {
     span.addEventListener('click', function() {
         document.querySelectorAll('.number').forEach(s => s.style.backgroundColor = '');
+        document.querySelectorAll('.number').forEach(s => s.style.color = '');
         span.style.backgroundColor = 'white';
         span.style.color = 'black';
     });
